de81c6aa8d2f01e011e6b1cd6059b2d52db04b4a
Empire <empirephoenix@yahoo.de>
2022-02-14 02:25:20 +0100

    rmt rc receiver test

---
+++ esp32test/Esp32DeepSleepTest/include/joy_rmt_rc.h
@@ -0,0 +1,113 @@
+/*
+
+Using ESP32 RMT to read servo pulses sent by radio remote control receiver.
+Publishes result to a queue of joy_msg.
+
+Reference: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/rmt.html
+Reference: https://github.com/JustinOng/sumo/blob/master/software/sumo/src/configure_rmt.c
+  (via https://www.reddit.com/r/esp32/comments/bivbz1/can_a_esp32_read_the_pwm_signal_of_a_rc_receiver/)
+
+Copyright (c) Roger Cheng
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+#ifndef INC_JOY_RMT_RC_H
+#define INC_JOY_RMT_RC_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+
+#include "driver/rmt.h"
+
+#define axis_count 2
+
+/*
+ * @brief Structure to hold RMT configuration information specific to a channel
+ */
+typedef struct rmt_rc_channel
+{
+  rmt_channel_t channel;
+  gpio_num_t pin;
+} rmt_rc_channel;
+
+/*
+ * @brief Array of channel-specific RMT configuration parameters
+ */
+static const rmt_rc_channel rc_channels[axis_count] = {
+  {
+    RMT_CHANNEL_0,
+    GPIO_NUM_14,
+  }
+  ,
+  {
+    RMT_CHANNEL_1,
+    GPIO_NUM_12,
+  } /*,
+  {
+    RMT_CHANNEL_2,
+    GPIO_NUM_13,
+  },
+    {
+    RMT_CHANNEL_3,
+    GPIO_NUM_5,
+  }*/
+};
+
+/*
+ * @brief Divide RMT 80MHz by 80 = 1MHz = Each tick is 1us (microsecond)
+ */
+static const uint8_t rmt_clock_divider = 80;
+
+/*
+ * @brief Go into idle after inaction longer than 7ms = 7000us
+ */
+static const uint16_t rmt_idle_threshold = 7000;
+
+/*
+ * @brief Signal shorter than 0.25ms = 250us will be ignored as noise
+ */
+static const uint8_t rmt_filter_threshold = 250;
+
+/*
+ * @brief RC receiver usually sends 1ms = 1000us on the low end
+ */
+static const uint16_t rc_receive_min = 1000;
+
+/*
+ * @brief RC receiver usually sends 1.5ms = 1500us as center position
+ */
+static const uint16_t rc_receive_mid = 1500;
+
+/*
+ * @brief RC receiver usually sends 2ms = 2000us on the high end
+ */
+static const uint16_t rc_receive_max = 2000;
+
+/*
+ * @brief FreeRTOS task which reads servo control signals sent by radio
+ * remote control receiver and translate them to joystick message.
+ * @param pvParam QueueHandle to joy_msg mailbox.
+ */
+
+void joy_rmt_rc_read_task(void* pvParam);
+
+#endif // #ifndef INC_JOY_RMT_RC_H
--- esp32test/Esp32DeepSleepTest/src/main.cpp
+++ esp32test/Esp32DeepSleepTest/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
-#include "menu_menu.h"
+#include "joy_rmt_rc.h"
 
 #define LED_STATUS 32
 #define BUZZER 27
@@ -28,9 +28,9 @@ const int M1_A_CHANNEL = 0;
 void setup()
 {
 
-  Serial.begin(115200);
 
-  setupMenu();  
+  Serial.begin(115200);
+ 
   pinMode(ONE_WIRE, INPUT);
 
   pinMode(M1_S1, INPUT);
@@ -65,15 +65,118 @@ void setup()
 
 }
 
+
+void check(bool value, char* output)
+{
+  if (value)
+  {
+    printf(output);
+  }
+}
+
+void joy_rmt_rc_read_task()
+{
+  esp_err_t er = ESP_OK;
+
+
+  // Configure RMT peripheral for each RC channel
+  RingbufHandle_t ringbuffers[axis_count];
+
+  for (int i = 0; i < axis_count; i++)
+  {
+    rmt_config_t rmt_rx_config = {};
+    rmt_rx_config.rmt_mode = RMT_MODE_RX;
+    rmt_rx_config.channel = rc_channels[i].channel;
+    rmt_rx_config.clk_div = rmt_clock_divider;
+    rmt_rx_config.gpio_num = rc_channels[i].pin;
+    rmt_rx_config.mem_block_num = 1;
+    rmt_rx_config.rx_config.filter_en = true;
+    rmt_rx_config.rx_config.filter_ticks_thresh = rmt_filter_threshold;
+    rmt_rx_config.rx_config.idle_threshold = rmt_idle_threshold;
+
+
+    er = rmt_config(&rmt_rx_config);
+    check(ESP_OK != er, "ERROR: rmt_config failed\n");
+
+    er = rmt_driver_install(rc_channels[i].channel, 1024, 0);
+    check(ESP_OK != er, "ERROR: rmt_driver_install failed\n");
+
+    er = rmt_get_ringbuf_handle(rc_channels[i].channel, &(ringbuffers[i]));
+    check(ESP_OK != er, "ERROR: rmt_get_ringbuf_handle failed\n");
+    check(NULL == ringbuffers[i], "ERROR: rmt_get_ringbuf_handle gave a NULL handle\n");
+
+    er = rmt_rx_start(rc_channels[i].channel, true);
+    check(ESP_OK != er, "ERROR: rmt_rx_start failed\n");
+  }
+
+  // Read loop
+  size_t length = 0;
+  rmt_item32_t *items = NULL;
+  uint32_t duration;
+  while(true)
+  {
+    for (int i = 0; i < axis_count; i++)
+    {
+      duration = 0;
+      items = (rmt_item32_t *)xRingbufferReceive(ringbuffers[i], &length, portMAX_DELAY);
+      if (NULL != items)
+      {
+        // Convert length from number of bytes to number of entries
+        length /= sizeof(rmt_item32_t);
+
+        // Read high period of most recent data point
+        rmt_item32_t recent = items[length-1];
+        if (0 != recent.level0)
+        {
+          duration = recent.duration0;
+        }
+        else if (0 != recent.level1)
+        {
+          duration = recent.duration1;
+        }
+
+        // Return memory to ring buffer
+        vRingbufferReturnItem(ringbuffers[i], (void*)items);
+
+        // Clamp between min & max
+        if (duration < rc_receive_min)
+        {
+          duration = rc_receive_min;
+        }
+        else if (duration > rc_receive_max)
+        {
+          duration = rc_receive_max;
+        }
+
+        // Convert range from (rc_receive_min, rc_receive_max) to (-1, 1)
+        Serial.print(i);
+        Serial.print(":");
+        Serial.println(-1.0 + 2.0*((float)duration-rc_receive_min)/(rc_receive_max-rc_receive_min));
+      }
+      else
+      {
+        // No data? Failsafe to center.
+        Serial.print(i);
+        Serial.print(":");
+        Serial.println(0.0);
+      }
+    }
+
+    Serial.print("Tasktime");
+    Serial.println(xTaskGetTickCount());
+
+
+  }
+}
+
 void loop()
 {
-  taskManager.runLoop();
 
-  int pwmin = pulseIn(M1_S1, HIGH, 200000);
-  int pwm_clean = constrain(pwmin, 1000, 2000);
-  int out_duty_cycle = map(pwm_clean,1000,2000,20,5000);
-  Serial.println(out_duty_cycle);
+  joy_rmt_rc_read_task();
+  //int pwmin = pulseIn(M1_S1, HIGH, 200000);
+  //int pwm_clean = constrain(pwmin, 1000, 2000);
+  //int out_duty_cycle = map(pwm_clean,1000,2000,20,5000);
+  //Serial.println(out_duty_cycle);
   //ledcWrite(M1_A_CHANNEL, out_duty_cycle);
   //digitalWrite(M1_A_LO, HIGH);
-  delay(100);
 }
